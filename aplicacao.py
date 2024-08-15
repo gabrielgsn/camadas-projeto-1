66777d034f05867d0c6e57596ff6d617ea40c474
gabrielgsn <ggsnasser@gmail.com>
2024-08-15 15:30:51 -0300

    final

--- aplicacao.py
+++ aplicacao.py
@@ -23,7 +23,7 @@ import sys
 #use uma das 3 opcoes para atribuir à variável a porta usada
 #serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
 #serialName = "/dev/tty.usbmodem1411" # Mac    (variacao de)
-serialName = "COM4"                  # Windows(variacao de)
+serialName = "COM3"                  # Windows(variacao de)
 
 
 def main():
